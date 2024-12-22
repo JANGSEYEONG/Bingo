6c1cbb09f493218a4d672c2ce84add95c6464a13
jangseyeong <jang.se.yeong000@gmail.com>
2024-12-22 23:19:41 +0900

    chore: alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@stores', replacement: path.resolve(__dirname, 'src/stores') },
+      { find: '@lib', replacement: path.resolve(__dirname, 'src/lib') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+    ],
   },
 });
