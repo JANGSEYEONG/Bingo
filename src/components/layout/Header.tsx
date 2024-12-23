561ebef78e9a1c889fe94ae0ace1bc1473da1904
jangseyeong <jang.se.yeong000@gmail.com>
2024-12-23 19:35:59 +0900

    feat: 게임 시작 시, 홈으로 돌아가는 버튼 삭제

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,6 @@
-// Header.tsx
-import { Home, PartyPopper } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { useNavigate, useLocation } from 'react-router';
+import { PartyPopper } from 'lucide-react';
 
 const Header = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const isHome = location.pathname === '/';
-
   return (
     <header className="border-b">
       <div className="mx-auto w-full px-4 py-4">
@@ -16,11 +9,6 @@ const Header = () => {
             <PartyPopper className="h-5 w-5 text-yellow-500" />
             <span className="font-bold">2024 GN 종무식</span>
           </div>
-          {!isHome && (
-            <Button variant="outline" size="sm" onClick={() => navigate('/')}>
-              <Home className="h-5 w-5" />
-            </Button>
-          )}
         </div>
       </div>
     </header>
